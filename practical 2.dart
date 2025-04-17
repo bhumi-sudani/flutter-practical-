f6c73fc632c66d032193742bbb2a26ce552fdfe4
bhumi-sudani <bhumisudani96@gmail.com>
2025-04-17 08:01:34 +0530

    practical 2.dart

---
+++ practical 2.dart
@@ -0,0 +1,6 @@
+import'package:flutter/material.dart'; void main()
+{
+runApp( MaterialApp(debugShowCheckedModeBanner: false, home:Text("hello Bhumi",
+style: TextStyle(color: Colors.blueAccent),),)
+);
+}
